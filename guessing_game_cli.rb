5ce8ab274a8f2b8b50f2468dd0900519fb5af5d7
Billy Helm <billyhelm88@gmail.com>
2020-08-05 21:47:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   num = rand(5)+1
   puts "Guess a random number between 1 and 6"
-  input = gets
+  input = gets.chomp
   if num == input 
     puts "You guessed the correct number!" 
   elsif input == exit 
